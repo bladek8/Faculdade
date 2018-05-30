82ffe2a73cbfd458afb90b2e0b6fe74c4a84e728
HenriqueCaue <henrique_caue@hotmail.com>
2018-05-29 22:06:04 -0300

    Checkpoint

---
+++ Assets/menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+    public void start()
+    {
+        //Debug.Log("Mudou a cena");
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void sair()
+    {
+        //Debug.Log("Fechou o jogo");
+        Application.Quit();
+    }
+}
