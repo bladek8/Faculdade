f5a84f5945e776415a690bef1c9c6b30127d9197
bladek8 <lucas1999araujo@gmail.com>
2018-06-06 23:12:36 -0300

    melhorias

--- Assets/Painel.cs
+++ Assets/Painel.cs
@@ -9,33 +9,23 @@ public class Painel : MonoBehaviour
     public Transform painel;
     Mouse script;
     public Transform fundo;
-    bool isClicked = false;
     Transform painelClone;
+    public BoxCollider2D bx;
 
     private void Start()
     {
+        bx.GetComponent<BoxCollider2D>();
         camera = Camera.main;
         script = camera.GetComponent<Mouse>();
     }
 
     private void OnMouseDown()
     {
-        if (!isClicked)
-        {
-            script.frezzed = true;
-            isClicked = true;
-            painelClone = Instantiate(painel, new Vector3(-0.067f, 1.13f, -9), Quaternion.identity);
-            fundo.position = new Vector3(fundo.position.x, fundo.position.y, -3);
-            camera.orthographicSize -= 4;
-            camera.transform.position = new Vector3(camera.transform.position.x, 1.2f, camera.transform.position.z);
-        }
-        else
-        {
-            //script.frezzed = false;
-            //isClicked = false;
-            //fundo.position = new Vector3(fundo.position.x, fundo.position.y, 0);
-            //camera.orthographicSize += 4;
-            //Destroy(painelClone.gameObject);
-        }
+        bx.enabled = false;
+        script.frezzed = true;
+        painelClone = Instantiate(painel, new Vector3(-0.067f, 1.13f, -9), Quaternion.identity);
+        fundo.position = new Vector3(fundo.position.x, fundo.position.y, -3);
+        camera.orthographicSize -= 4;
+        camera.transform.position = new Vector3(camera.transform.position.x, 1.2f, camera.transform.position.z);
     }
 }
---
+++ Assets/scriptpainell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptpainell : MonoBehaviour
+{
+
+    Camera camera;
+    Mouse script;
+    public Painel pnl;
+    public Transform fundo;
+    Transform painelClone;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        script = camera.GetComponent<Mouse>();
+    }
+
+    private void OnMouseDown()
+    {
+        pnl.bx.enabled = true;
+        script.frezzed = false;
+        fundo.position = new Vector3(fundo.position.x, fundo.position.y, 0);
+        camera.orthographicSize += 4;
+        Destroy(gameObject);
+    }
+}
